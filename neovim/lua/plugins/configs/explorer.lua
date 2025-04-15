63cf8c1e612b7db46454f8b23f22a41fc001537c
Martin Bøge Jørgensen <martin@Martins-MacBook-Air.local>
2025-04-15 09:56:43 +0200

    feat: neovim explorer shortcuts

--- neovim/lua/plugins/configs/explorer.lua
+++ neovim/lua/plugins/configs/explorer.lua
@@ -11,15 +11,32 @@ return {
 
 		require("nvim-tree").setup({
 			view = {
-				width = 40,
+				width = 60,
 			},
 		})
 
 		local keymap = vim.keymap.set
 
-		keymap("n", "<leader>ef", "<cmd>NvimTreeFocus<CR>")
-		keymap("n", "<leader>et", "<cmd>NvimTreeToggle<CR>")
-		keymap("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>")
-		keymap("n", "<C-w>o", "<C-w>o<cmd>NvimTreeToggle<CR><C-w>l><ESC>")
+		keymap("n", "<leader>ef", "<cmd>NvimTreeFocus<CR>", { desc = "Explorer: Focus" })
+		keymap("n", "<leader>et", "<cmd>NvimTreeToggle<CR>", { desc = "Explorer: Toggle visibility" })
+		keymap("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Explorer: Collapse all folders" })
+		keymap(
+			"n",
+			"<C-w>o",
+			"<C-w>o<cmd>NvimTreeToggle<CR><C-w>l><ESC>",
+			{ desc = "Close all but current window and tree" }
+		)
+		keymap(
+			"n",
+			"<leader>ew",
+			"<cmd>NvimTreeFocus<CR><cmd>vertical resize +10<CR>",
+			{ desc = "Explorer: Widen by 10 columns" }
+		)
+		keymap(
+			"n",
+			"<leader>en",
+			"<cmd>NvimTreeFocus<CR><cmd>vertical resize -10<CR>",
+			{ desc = "Explorer: Narrow by 10 columns" }
+		)
 	end,
 }
