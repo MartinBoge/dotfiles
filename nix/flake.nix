2eb8f3437f0885b3fb2c82f1077b8c657d9476e1
Martin Bøge Jørgensen <martin.boge@live.dk>
2026-01-04 01:41:32 +0000

    nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,20 +2,20 @@
   description = "Martin's portable Nix config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
     nixos-wsl = {
-      url = "github:nix-community/nixos-wsl/release-25.05";
+      url = "github:nix-community/nixos-wsl/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
