dffa99c28104b504c5ca658fbf50f30c6b712373
Martin Bøge Jørgensen <martin.boge@live.dk>
2026-01-04 01:29:55 +0000

    feat: nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
     nixos-wsl = {
-      url = "github:nix-community/NixOS-WSL";
+      url = "github:nix-community/nixos-wsl/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
