7cfa1e551bbc48fc093393883c8d300707085068
Martin Bøge Jørgensen <martin.boge@live.dk>
2026-03-11 12:15:00 +0100

    Add markdown previewer to Neovim

--- neovim/init.lua
+++ neovim/init.lua
@@ -353,6 +353,15 @@ require("lazy").setup({
 			main = "ibl",
 			opts = {},
 		},
+		{
+			"iamcco/markdown-preview.nvim",
+			cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+			build = "cd app && npm install",
+			init = function()
+				vim.g.mkdp_filetypes = { "markdown" }
+			end,
+			ft = { "markdown" },
+		},
 	},
 })
 
